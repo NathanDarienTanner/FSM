90c3504a08448208f293021510acc1ff18220985
NathanDarienTanner <114546421+NathanDarienTanner@users.noreply.github.com>
2023-01-11 17:05:10 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,398 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "mylib/mylib.h"
+
+void car_moving() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|_                |\n");
+    printf("| | _             |\n");
+    printf("|_ __|            |\n");
+    printf("|_0_______________|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving1() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|___              |\n");
+    printf("|   | _           |\n");
+    printf("|___ __|          |\n");
+    printf("|___0_____________|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving2() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|  ___            |\n");
+    printf("|_|   |__         |\n");
+    printf("|_ ___ __|        |\n");
+    printf("|_0___0___________|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving3() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|    ___          |\n");
+    printf("| __|   |__       |\n");
+    printf("||__ ___ __|      |\n");
+    printf("|___0___0_________|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving4() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|       ___       |\n");
+    printf("|    __|   |__    |\n");
+    printf("|   |__ ___ __|   |\n");
+    printf("|______0___0______|\n");
+    printf("   State : Moving\n");
+}
+
+
+
+void car_moving5() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|           ___   |\n");
+    printf("|        __|   |__|\n");
+    printf("|       |__ ___ __|\n");
+    printf("|__________0___0__|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving6() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|             ___ |\n");
+    printf("|          __|   ||\n");
+    printf("|         |__ ___ |\n");
+    printf("|____________0___0|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving7() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|               __|\n");
+    printf("|            __|  |\n");
+    printf("|           |__ __|\n");
+    printf("|______________0__|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving8() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|              __||\n");
+    printf("|             |__ |\n");
+    printf("|________________0|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving9() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                _|\n");
+    printf("|               |_|\n");
+    printf("|_________________|\n");
+    printf("   State : Moving\n");
+}
+
+void car_moving10() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|_________________|\n");
+    printf("   State : Moving\n");
+}
+
+void car_braking() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|_                |\n");
+    printf("| | _             |\n");
+    printf("|_ __|            |\n");
+    printf("|_0_______________|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking1() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|___              |\n");
+    printf("|   | _           |\n");
+    printf("|___ __|          |\n");
+    printf("|___0_____________|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking2() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|  ___            |\n");
+    printf("|_|   |__         |\n");
+    printf("|_ ___ __|        |\n");
+    printf("|_0___0___________|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking3() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|    ___          |\n");
+    printf("| __|   |__       |\n");
+    printf("||__ ___ __|      |\n");
+    printf("|___0___0_________|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking4() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|       ___       |\n");
+    printf("|    __|   |__    |\n");
+    printf("|   |__ ___ __|   |\n");
+    printf("|______0___0______|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking5() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|         ___     |\n");
+    printf("|      __|   |__  |\n");
+    printf("|     |__ ___ __| |\n");
+    printf("|________0___0____|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking6() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|           ___   |\n");
+    printf("|        __|   |__|\n");
+    printf("|       |__ ___ __|\n");
+    printf("|__________0___0__|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking7() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|             ___ |\n");
+    printf("|          __|   ||\n");
+    printf("|         |__ ___ |\n");
+    printf("|____________0___0|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking8() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|               __|\n");
+    printf("|            __|  |\n");
+    printf("|           |__ __|\n");
+    printf("|______________0__|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking9() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|              __||\n");
+    printf("|             |__ |\n");
+    printf("|________________0|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking10() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                _|\n");
+    printf("|               |_|\n");
+    printf("|_________________|\n");
+    printf("   State : Braking\n");
+}
+
+void car_braking11() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|_________________|\n");
+    printf("   State : Braking\n");
+}
+
+void car_stopped() {
+    printf(" _________________\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|                 |\n");
+    printf("|       ___       |\n");
+    printf("|    __|   |__    |\n");
+    printf("|   |__ ___ __|   |\n");
+    printf("|______0___0______|\n");
+    printf("  State : Stopped\n");
+}
+
+int main(void) {
+    int s0 = 0, s1 = 0, D, O = 0;
+    while (true) {
+        printf("\033[2J");
+        printf("\033[1;1H");
+        if (s0 == 0 && s1 == 0) {
+            int i;
+            for (i = 12; i>1; i--){
+                if (i == 12){
+                    car_moving();
+                }
+                if (i == 11){
+                    car_moving1();
+                    
+                }
+                if (i == 10){
+                    car_moving2();
+                }
+                if (i == 9){
+                    car_moving3();
+                }
+                if (i == 8){
+                    car_moving4();
+                }
+                if (i == 7){
+                    car_moving5();
+                }
+                if (i == 6){
+                    car_moving6();
+                }
+                if (i == 5){
+                    car_moving7();
+                }
+                if (i == 4){
+                    car_moving8();
+                }
+                if (i == 3){
+                    car_moving9();
+                }
+                if (i == 2){
+                    car_moving10();
+                }
+                sleep(1);
+                printf("\033[2J");
+                printf("\033[1;1H");
+            }
+        } else if (s0==0 && s1==1) {
+                        int i;
+            for (i = 12; i>1; i--){
+                if (i == 12){
+                    car_braking();
+                }
+                if (i == 11){
+                    car_braking1();
+                    
+                }
+                if (i == 10){
+                    car_braking2();
+                }
+                if (i == 9){
+                    car_braking3();
+                }
+                if (i == 8){
+                    car_braking4();
+                }
+                if (i == 7){
+                    car_braking5();
+                }
+                if (i == 6){
+                    car_braking6();
+                }
+                if (i == 5){
+                    car_braking7();
+                }
+                if (i == 4){
+                    car_braking8();
+                }
+                if (i == 3){
+                    car_braking9();
+                }
+                if (i == 2){
+                    car_braking10();
+                }
+                sleep(1.5);
+                printf("\033[2J");
+                printf("\033[1;1H");
+            }
+        } else {
+            car_stopped();
+        }
+        printf("Enter button input (0 if no danger is detected // 1 if danger is detected): ");
+        scanf("%d", &D);
+
+
+        if (D == 0 || D == 1) {
+            states(s0, s1, D, &s0, &s1, &O);
+        } else {
+            printf("Please enter a proper input (0 or 1)\n");
+        }
+    }
+    return 0;
+}
+
